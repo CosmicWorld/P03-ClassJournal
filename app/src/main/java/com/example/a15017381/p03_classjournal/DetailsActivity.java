3a66472821bdbd143344e2986a050356f903bb3f
SamanthaFang <13045249@myrp.edu.sg>
2017-05-05 15:48:13 +0800

    hi

--- app/src/main/java/com/example/a15017381/p03_classjournal/DetailsActivity.java
+++ app/src/main/java/com/example/a15017381/p03_classjournal/DetailsActivity.java
@@ -1,9 +1,18 @@
 package com.example.a15017381.p03_classjournal;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 /**
  * Created by 15017381 on 5/5/2017.
  */
 
 public class DetailsActivity {
+
+
+
 
 }
--- app/src/main/java/com/example/a15017381/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/example/a15017381/p03_classjournal/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.a15017381.p03_classjournal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnRP;
 
     ListView lvModule;
     ArrayAdapter aa;
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         lvModule = (ListView) findViewById(R.id.lvModule);
 
@@ -39,7 +44,25 @@ public class MainActivity extends AppCompatActivity {
                         DetailsActivity.class);
                 i.putExtra("module", al.get(position).getCode());
                 startActivity(i);
+
+                btnRP = (Button) findViewById(R.id.buttonInfo);
+                btnRP.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View arg0) {
+                        // Intent to display data
+                        Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                        // Set the URL to be used.
+                        rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                        startActivity(rpIntent);
+
+                    }
+                });
             }
+
+
+
+        }
         });
     }
 }
